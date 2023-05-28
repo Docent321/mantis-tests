4d9f5b24d692f7b8c16bd87e49104bc86dfce8b5
Alex_Donskov <mrad@list.ru>
2023-05-28 03:49:08 +0300

    Развернул структуру для тестов Мантис

---
+++ src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.mantis.tests;
+
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+
+public class TestBase {
+
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", "CHROME"));
+
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
